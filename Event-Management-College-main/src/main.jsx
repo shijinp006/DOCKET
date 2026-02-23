51a83229a9bafe57a9cd7be8cf763619ac5bf226
jagathkrishna123 <jagathkrishna900@gmail.com>
2026-02-23 14:31:43 +0530

    last commit

--- Event-Management-College-main/src/main.jsx
+++ Event-Management-College-main/src/main.jsx
@@ -5,12 +5,18 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
--- Event-Management-College-main/vite.config.js
+++ Event-Management-College-main/vite.config.js
@@ -2,8 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),svgr()],
+  plugins: [react(), tailwindcss(), svgr(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg'],
+    manifest: {
+      name: 'Attendance QR App',
+      short_name: 'Attendance',
+      description: 'Employee Attendance QR Scanner',
+      theme_color: '#0f172a',
+      background_color: '#0f172a',
+      display: 'standalone',
+      orientation: 'portrait',
+      start_url: '/',
+      scope: '/',
+      icons: [
+        {
+          src: '/pwa-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })]
 })
